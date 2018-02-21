c0bb243a206dd7e3edf23ce7a73d6a2c5ef5d3aa
Alikhan <abakhitbek@gmail.com>
2018-02-22 01:22:13 +0600

    main page changes

--- project/src/Home.js
+++ project/src/Home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Header from './Header';
-
+import images from './images';
 
 class Home extends Component {
   render() {
@@ -18,6 +18,83 @@ class Home extends Component {
             </div>
           </div>
         </header>
+
+
+        <div className = "wrapper">
+            <div className = "news">
+              <h4 className = "news-header">Новости</h4>
+              
+              <div className = "news-grid">
+                  <div className = "row-news">
+                    <div className = "column-news">
+                      <div className = "image-container">
+                        <img className="image-news" alt = "" src = {images.gs}/>
+                        <div className="image-data">
+                          <div className="image-data-text">
+                            John Doe
+                          </div>
+                        </div>
+                      </div>
+                      <div className = "image-container">
+                        <img className="image-news" alt = "" />
+                        <div className="image-data">
+                          <div className="image-data-text">
+                            John Doe
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className = "column-news">
+                      <div className = "image-container">
+                        <img className="image-news" alt = "" src = {images.jt2}/>
+                        <div className="image-data">
+                          <div className="image-data-text">
+                            John Doe
+                          </div>
+                        </div>
+                      </div>
+                      <div className = "image-container">
+                        <img className="image-news" alt = "" src = {images.khn}/> 
+                        <div className="image-data">
+                          <div className="image-data-text">
+                            John Doe
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
+
+                
+            <div className = "charts">
+              <h4 className = "charts-header">Чарты</h4>
+                  
+              <div className = "charts-grid">
+                  <div className = "row-charts">
+                      <div className = "column-charts-left">
+                          <img className = "image-charts-big" alt = "" src = {images.soary}/>
+                          <div className = "column-charts-left-data">
+                              <p className = "column-charts-left-data-chart-name">Spotify Top 200</p>
+                              <p className = "column-charts-left-data-singer-name">Drake</p>
+                              <p className = "column-charts-left-data-song-name">God's Plan</p>
+                          </div>
+                      </div>
+
+                      <div className = "column-charts-right">
+                          <h4 className = "billboard-header">Billboard</h4>
+                      </div>
+                  </div>
+
+
+              </div>
+            </div>
+
+        </div>
+
+
+
+
       </div>
     );
   }
---
+++ project/src/images.js
@@ -0,0 +1,12 @@
+import gs from './img/greatest_showman.png'
+import jt from './img/jt.jpg'
+import jt2 from './img/jt2.png'
+import khn from './img/khn.png'
+import soary from './img/soary.png'
+export default{
+	gs,
+	jt,
+	jt2,
+	khn,
+	soary
+}
