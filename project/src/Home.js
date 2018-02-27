9aef6b4351795e1d37fe7985b921dd72f0a00b82
Alikhan <abakhitbek@gmail.com>
2018-02-27 22:07:14 +0600

    changes in homepage

--- project/src/Home.js
+++ project/src/Home.js
@@ -27,37 +27,28 @@ class Home extends Component {
               <div className = "news-grid">
                   <div className = "row-news">
                     <div className = "column-news">
-                      <div className = "image-container">
-                        <img className="image-news" alt = "" src = {images.gs}/>
-                        <div className="image-data">
-                          <div className="image-data-text">
-                            John Doe
-                          </div>
+                        <div className = "column-news-left">
+                          <img className="image-news-big" src = {images.gs}/>
+                          <h3 className = "image-news-big-data">Сборник саундтреков к фильму "Величайший шоумен" остается на вершине
+                          британского чарта</h3>
                         </div>
-                      </div>
-                      <div className = "image-container">
-                        <img className="image-news" alt = "" />
-                        <div className="image-data">
-                          <div className="image-data-text">
-                            John Doe
-                          </div>
-                        </div>
-                      </div>
                     </div>
                     <div className = "column-news">
-                      <div className = "image-container">
-                        <img className="image-news" alt = "" src = {images.jt2}/>
-                        <div className="image-data">
-                          <div className="image-data-text">
-                            John Doe
+                      <div className = "column-news-right">
+                        <div className = "image-container">
+                          <img className="image-news" alt = "" src = {images.jt2}/>
+                          <div className="image-data">
+                            <div className="image-data-text">
+                              Джастин Тимберлейк возглавил альбомный чарт США
+                            </div>
                           </div>
                         </div>
-                      </div>
-                      <div className = "image-container">
-                        <img className="image-news" alt = "" src = {images.khn}/> 
-                        <div className="image-data">
-                          <div className="image-data-text">
-                            John Doe
+                        <div className = "image-container">
+                          <img className="image-news" alt = "" src = {images.khn}/> 
+                          <div className="image-data">
+                            <div className="image-data-text">
+                              Khalid и Normani записали дуэт ко Дню Всех Влюбленных
+                            </div>
                           </div>
                         </div>
                       </div>
@@ -83,6 +74,40 @@ class Home extends Component {
 
                       <div className = "column-charts-right">
                           <h4 className = "billboard-header">Billboard</h4>
+                          
+
+                          <div className = "billboard-list">
+                            <div className = "billboard-list-1">
+                              <img className = "billboard-list-1-photo" alt = "" src = {images.drake}/>
+                              <div className = "billboard-list-data">
+                                <p className = "billboard-list-1-chartname">Billboard Hot 100</p>
+                                <p className = "billboard-list-1-songname">God's Plan</p>
+                                <p className = "billboard-list-1-singername">Drake</p>
+                              </div>
+                            </div>
+
+                            <div className = "billboard-list-2">
+                              <img className = "billboard-list-2-photo" alt = "" src = {images.migos}/>
+                              <div className = "billboard-list-data">
+                                <p className = "billboard-list-2-chartname">Billboard 200</p>
+                                <p className = "billboard-list-2-songname">Culture</p>
+                                <p className = "billboard-list-2-singername">Migos</p>
+                              </div>
+                            </div>
+
+                            <div className = "billboard-list-3">
+                              <img className = "billboard-list-3-photo" alt = "" src = {images.sheeran}/>
+                              <div className = "billboard-list-data">
+                                <p className = "billboard-list-3-chartname">Radio songs</p>
+                                <p className = "billboard-list-3-songname">Perfect</p>
+                                <p className = "billboard-list-3-singername">Ed Sheeran</p>
+                              </div>
+                            </div>
+
+
+                            <h3 className = "billboard-all-charts">Все чарты</h3>
+
+                          </div>
                       </div>
                   </div>
 
@@ -93,6 +118,202 @@ class Home extends Component {
         </div>
 
 
+        <div className = "videos-block">
+          <h4 className = "videos-header">Видео</h4>
+
+          <div className = "videos-container">
+            <div className = "col-md-4">
+              <img className = "video-image-1" src = {images.maroon}/>
+              <h3 className = "video-text-1">Maroon 5 - Wait(Official Video)</h3>
+            </div>
+
+            <div className = "col-md-4">
+              <img className = "video-image-2" src = {images.geazy}/>
+              <h3 className = "video-text-2">G-Eazy - No Limit feat. Cardi B(Official Video)</h3>
+            </div>
+
+            <div className = "col-md-4">
+              <img className = "video-image-3" src = {images.fifthharmony}/>
+              <h3 className = "video-text-3">Fifth Harmony - Work from Home ft. Ty Dolla $ign</h3>
+            </div>
+          </div>
+
+        </div>
+
+
+
+        <div className = "wrapper">
+
+          <div className = "music-block">
+            <h4 className = "music-block-header">Музыка</h4>
+          
+            <div className = "music-block-container">
+              <div className = "music-news-1">
+                <h3 className = "music-news-title">Премьера нового сингла Inna</h3>
+                <p className = "music-news-time">три часа назад</p>
+
+                <p className = "music-news-body">
+                Официальная обложка нового сингла Инны - "Me Gusta", релиз которого
+                ожидается на этой неделе. Песня войдет во вторую часть пятого студийного
+                альбома певицы "Nirvana".</p>
+
+                <img className = "music-news-pic-big" src = {images.inna} alt = ""/>
+              
+                <div className = "actions-container">
+                  <div className = "like-button-container">
+                    <p className = "like-button-text">Нравится</p>
+                    <p className = "like-button-count">169</p>
+                  </div>
+
+                  <div className = "comments-button-container">
+                    <p className = "comments-button-text">Комментировать</p>
+                  </div>
+                </div>
+              </div>
+
+              <hr className = "divider"/>
+
+              <div className = "music-news-2">
+                <h3 className = "music-news-title">"One Call Away" - 500 million</h3>
+                <p className = "music-news-time">вчера в 16:38</p>
+
+                <p className = "music-news-body">
+                Видеоклип на хит-сингл Чарли Пута - "One Call Away"
+                преодолел порог в 500,000,000 просмотров на "YouTube". 
+                Это пятое видео исполнителя, достигшее такого результата.</p>
+
+                <div className = "music-news-2-pic-container">
+                  <img classname = "news-2-pic-1" src = {images.charlie1} alt = ""/>
+                  <img classname = "news-2-pic-2" src = {images.charlie2} alt = ""/>
+                </div>
+                <div className = "actions-container">
+                  <div className = "like-button-container">
+                    <p className = "like-button-text">Нравится</p>
+                    <p className = "like-button-count">169</p>
+                  </div>
+
+                  <div className = "comments-button-container">
+                    <p className = "comments-button-text">Комментировать</p>
+                  </div>
+                </div>
+              </div>
+
+              <hr className = "divider"/>
+
+              <h4 className = "music-block-more">Больше музыки</h4>
+
+            </div>
+
+
+          </div>
+
+
+          <div className = "facts-block">
+            <h4 className = "facts-block-header">Факты</h4>
+
+            <div className = "facts-block-container">
+              <div className = "facts-news-1">
+                <h3 className = "facts-news-title">Самые прослушиваемые песни в мировом "Spotify"</h3>
+                <p className = "facts-news-time">три часа назад</p>
+
+                <p className = "facts-news-body">
+                  Самые прослушиваемые песни в мировом "Spotify" на данный момент:  
+                </p>
+
+                <ol>
+                    <li>"God's Plan"</li>
+                    <li>"All The Stars"</li>
+                    <li>"Pray For Me"</li>
+                    <li>"rockstar"</li>
+                </ol>
+
+                
+                <div className = "facts-news-1-images-container">
+                    <img className = "facts-news-1-image-1" src = {images.soary}/>
+                    <div className = "facts-news-1-images-small-container">
+                      <img src = {images.allthestars}/>
+                      <br/>
+                      <img src = {images.pray4me}/>
+                      <br/>
+                      <img src = {images.rockstar}/>
+                      <br/>
+                    </div>
+
+                </div>
+
+
+                <div className = "actions-container">
+                  <div className = "like-button-container">
+                    <p className = "like-button-text">Нравится</p>
+                    <p className = "like-button-count">169</p>
+                  </div>
+
+                  <div className = "comments-button-container">
+                    <p className = "comments-button-text">Комментировать</p>
+                  </div>
+                </div>
+              </div>
+
+              <hr className = "divider"/>
+
+              <div className = "facts-news-2">
+                <h3 className = "facts-news-title">"One Call Away" - 500 million</h3>
+                <p className = "facts-news-time">вчера в 16:38</p>
+
+                <p className = "facts-news-body">
+                   Видеоклип на хит-сингл Чарли Пута - "One Call Away"
+                  преодолел порог в 500,000,000 просмотров на "YouTube". 
+                  Это пятое видео исполнителя, достигшее такого результата.
+                </p>
+
+                <img src = {images.charliefact}/>v
+
+                <div className = "actions-container">
+                  <div className = "like-button-container">
+                    <p className = "like-button-text">Нравится</p>
+                    <p className = "like-button-count">169</p>
+                  </div>
+
+                  <div className = "comments-button-container">
+                    <p className = "comments-button-text">Комментировать</p>
+                  </div>
+                </div>
+              </div>
+
+              <hr className = "divider"/>
+
+              <h4 className = "facts-block-more">Больше фактов</h4>
+
+            </div>
+          </div>
+
+        </div>
+
+
+        <div className = "home-footer">
+          <div className = "footer-col-1">
+            <h1>ENERGY OF MUSIC</h1>
+            <p>Новостной портал о знаменитостях в яркой и сочной подаче. Исключительно
+            топовые и перспективные звезды мирового и российского шоу-бизнеса, а также новости
+            блогер-, бьюти-, киноиндустрии.</p>
+          </div>
+
+          <div className = "footer-col-2">
+            <h5>Связь с редакцией</h5>
+            <p>По вопросам сотрудничества и предложений:</p>
+            <p><a href = "#write">manager@popcake.tv</a></p>
+
+            <h5>По вопросам работы портала:</h5>
+            <p><a href = "#write">tech@popcake.tv</a></p>
+          </div>
+
+          <div className = "footer-col-3">
+            <h5>Подписка на новости</h5>
+            <p>Не хочешь пропустить важную новость про кумира? 
+            Тогда подпишись на нашу группу ВКонтакте, и мы оповестим тебя о новых материалах!</p>
+
+          </div>
+        </div>
 
 
       </div>
--- project/src/images.js
+++ project/src/images.js
@@ -3,10 +3,37 @@ import jt from './img/jt.jpg'
 import jt2 from './img/jt2.png'
 import khn from './img/khn.png'
 import soary from './img/soary.png'
+import drake from './img/drake.png'
+import migos from './img/migos.png'
+import sheeran from './img/sheeran.png'
+import maroon from './img/maroon.png'
+import geazy from './img/geazy.png'
+import fifthharmony from './img/fifthharmony.png'
+import inna from './img/inna.png'
+import charlie1 from './img/charlie1.png'
+import charlie2 from './img/charlie2.png'
+import allthestars from './img/allthestars.png'
+import pray4me from './img/pray4me.png'
+import rockstar from './img/rockstar.png'
+import charliefact from './img/charliefact.png'
+
 export default{
 	gs,
 	jt,
 	jt2,
 	khn,
-	soary
+	soary,
+	drake,
+	migos,
+	sheeran,
+	maroon,
+	geazy,
+	fifthharmony,
+	inna,
+	charlie1,
+	charlie2,
+	allthestars,
+	pray4me,
+	rockstar,
+	charliefact
 }
