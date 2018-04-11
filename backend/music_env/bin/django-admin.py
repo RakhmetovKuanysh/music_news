5a43fd768e2e93464567f35725ca1198725c5bce
RakhmetovKuanysh <kuanyshrakhmetov@gmail.com>
2018-04-11 14:50:14 +0600

    added backend folder

---
+++ backend/music_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kuanyshrakhmetov/Desktop/Kuanysh/ReactJS/music_project/backend/music_env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
